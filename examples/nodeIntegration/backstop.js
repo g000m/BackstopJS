d5a27adef8dda8e5bb7525099380138a2ae756e9
Gabe Herbert <gh@gabeherbert.com>
2019-06-27 20:25:00 -0700

    roughly adding ability to pass in config object

--- examples/nodeIntegration/backstop.js
+++ examples/nodeIntegration/backstop.js
@@ -1,25 +1,16 @@
 const _ = require('lodash');
-const fs = require('fs');
 const args = require('yargs').argv;
-const projectPath = `public/${args.p}`;
 const backstop = require('backstopjs');
 
-const filesToIgnore = {
-    "first-project": [
-        "ignore-me.html"
-    ],
-    "second-project": [
-        "ignore-me.html"
-    ],
-    "third-project": [
-        "ignore-me.html"
-    ]
-};
 
-const projectConfig = require("./backstop.config.js")({
+let projectConfig = require("./backstop.config.js")({
     "project": args.p,
-    "scenarios": getScenariosForProject(projectPath)
+    "scenarios": setScenariosForProject()
 });
+
+if( args.c ) {
+    projectConfig = JSON.parse(args.c);
+}
 
 let commandToRun = "";
 
@@ -39,39 +30,29 @@ if( "" !== commandToRun ) {
     backstop(commandToRun, { config: projectConfig });
 }
 
-function getScenariosForProject(projectPath) {
-    const files = fs.readdirSync(projectPath);
 
-    let scenarios;
+function setScenariosForProject() {
+    const urls = [
+        {
+            "label" : "Home",
+            "url" : "https://www.google.com/"
+        },
+        {
+            "label" : "Result",
+            "url" : "https://www.google.com/search?q=news"
+        }
+    ];
 
-    _.remove(files, isFileToIgnore);
-
-    scenarios = files.map(file => {
-        const scenarioLabel = file.split(".")[0].split("-").join(" ");
+    scenarios = urls.map(url => {
+        // const scenarioLabel = file.split(".")[0].split("-").join(" ");
 
         return {
-            "label": scenarioLabel,
-            "url": `http://localhost:8000/${projectPath}/${file}`,
-            "delay": 500,
+            "label": url.label,
+            "url": url.url,
+            "delay": 2000,
             "misMatchThreshold" : 0.1
         }
     });
 
     return scenarios;
-}
-
-function isFileToIgnore(file) {
-    let shouldBeRemoved = false;
-
-    //make sure we only have html files
-    if( file.indexOf(".html") === -1 ) {
-        shouldBeRemoved = true;
-    }
-
-    //exclude files by name
-    if( filesToIgnore[args.p] && filesToIgnore[args.p].indexOf(file) > -1 ) {
-        shouldBeRemoved = true;
-    }
-
-    return shouldBeRemoved;
 }
