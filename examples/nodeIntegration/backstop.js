924951867c0f5386ce68d1f2dd20a0e2bec97aba
Gabe Herbert <gh@gabeherbert.com>
2019-06-28 14:05:56 -0700

    [*] lint/prettify/refactor backstop.js

--- examples/nodeIntegration/backstop.js
+++ examples/nodeIntegration/backstop.js
@@ -1,58 +1,54 @@
-const _ = require('lodash');
+// const _ = require('lodash');
 const args = require('yargs').argv;
 const backstop = require('backstopjs');
 
-
-let projectConfig = require("./backstop.config.js")({
-    "project": args.p,
-    "scenarios": setScenariosForProject()
+let projectConfig = require('./backstop.config.js')({
+  'project': args.p,
+  'scenarios': setScenariosForProject()
 });
 
-if( args.c ) {
-    projectConfig = JSON.parse(args.c);
+if (args.c) {
+  projectConfig = JSON.parse(args.c);
 }
 
-let commandToRun = "";
+let commandToRun = '';
 
-if( args.reference ) {
-    commandToRun = "reference";
+if (args.reference) {
+  commandToRun = 'reference';
 }
 
-if( args.test ) {
-    commandToRun = "test";
+if (args.test) {
+  commandToRun = 'test';
 }
 
-if( args.openReport ) {
-    commandToRun = "openReport";
+if (args.openReport) {
+  commandToRun = 'openReport';
 }
 
-if( "" !== commandToRun ) {
-    backstop(commandToRun, { config: projectConfig });
+if (commandToRun !== '') {
+  backstop(commandToRun, { config: projectConfig });
 }
 
+function setScenariosForProject () {
+  const urls = [
+    {
+      'label': 'Home',
+      'url': 'https://www.google.com/'
+    },
+    {
+      'label': 'Result',
+      'url': 'https://www.google.com/search?q=news'
+    }
+  ];
 
-function setScenariosForProject() {
-    const urls = [
-        {
-            "label" : "Home",
-            "url" : "https://www.google.com/"
-        },
-        {
-            "label" : "Result",
-            "url" : "https://www.google.com/search?q=news"
-        }
-    ];
+  return urls.map(url => {
 
-    scenarios = urls.map(url => {
-        // const scenarioLabel = file.split(".")[0].split("-").join(" ");
+    return {
+      'label': url.label,
+      'url': url.url,
+      'delay': 2000,
+      'misMatchThreshold': 0.1
+    };
+  });
 
-        return {
-            "label": url.label,
-            "url": url.url,
-            "delay": 2000,
-            "misMatchThreshold" : 0.1
-        }
-    });
-
-    return scenarios;
 }
